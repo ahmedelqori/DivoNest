1a0c1a3aa2c7de3c108ce2995065e92a14b73c16
ahmedelqori <aelqori1@gmail.com>
2024-09-26 17:56:18 +0100

    Create User

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { UserModule } from '../user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UserModule } from '../user/user.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
   ],
   controllers: [AppController],
