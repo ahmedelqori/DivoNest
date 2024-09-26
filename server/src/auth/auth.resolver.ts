ae4dab6be945434bd2b9aa2c9175f5b777cb507a
ahmedelqori <aelqori1@gmail.com>
2024-09-26 18:40:04 +0100

    Create vite app

---
+++ server/src/auth/auth.resolver.ts
@@ -0,0 +1,13 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { Auth } from './entities/auth.entity';
+
+@Resolver(() => Auth)
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Query(() => String)
+  dummy() {
+    return '';
+  }
+}
